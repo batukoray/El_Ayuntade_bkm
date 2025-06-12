b5c11014c79ab3fd0a259985eec91e9d1cd51cb3
Batu Koray Masak <batukoraymasak@gmail.com>
2025-06-12 08:05:03 +0300

    Refactor command handling into analyze_input function for improved readability and maintainability

--- main.py
+++ main.py
@@ -48,6 +48,67 @@ commandoriginal = ''
 
 _orig_print = builtins.print
 
+def analyze_input(text):
+    global command, commandarr, commandoriginal
+    commandarr = [n for n in command.lower().split(' ') if n != '']
+    commandoriginal = command.strip()
+    command = command.lower().strip()
+    # If the command is empty, return
+    if command == '':
+        clear_last_lines(1)
+        return
+    # To Do App Commands:
+    if commandarr[0] == 'todo' and len(commandarr) > 1:
+        if commandarr[1] == 'help':
+            todo_help()
+        elif commandarr[1] == 'ls':
+            todo_ls()
+        elif commandarr[1] == 'rm':
+            if len(commandarr) > 2 and commandarr[2] == 'all':
+                todo_list.clear()
+                todo_save_todos()
+                print('All items were deleted.')
+            else:
+                todo_delete_function()
+        elif commandarr[1] == 'add':
+            todo_add()
+        elif commandarr[1] == 'changeorder':
+            todo_changeorder()
+        elif commandarr[1] == 'abcorder':
+            todo_abcorder()
+        elif commandarr[1] == 'cbaorder':
+            todo_cbaorder()
+        elif commandarr[1] == 'do':
+            todo_do_function()
+        else:
+            # Print the closest command if the command is not recognized
+            unknown_command()
+    elif commandarr[0] == 'todo' and len(commandarr) == 1:
+        todo_help()
+    elif commandarr[0] == 'open':
+        open_function()
+    elif commandarr[0] == 'help' and len(commandarr) == 1:
+        print(help_content)
+    elif commandarr[0] == 'chat' and len(commandarr) == 1:
+        chat_function()
+    elif (commandarr[0] == 'exit' or commandarr[0] == 'quit') and len(commandarr) == 1:
+        clear_screen()
+        print("".join(f"{neon_colors[i % len(neon_colors)]}{char}" for i, char in
+                      enumerate('\nGoodbye! | Robot Human Assist By: Batu Koray Masak')))
+        sys.exit(0)
+    elif (commandarr[0] == 'clear' or command == 'clr') and len(commandarr) == 1:
+        clear_screen()
+    elif commandarr[0] == 'eval':
+        try:
+            result = eval(command[5:].replace('pi', str(math.pi)).replace('e', str(math.e)))
+            if isinstance(result, (int, float)):
+                print(f"{result:,}")
+            else:
+                print(result)
+        except Exception:
+            unknown_command()
+    else:
+        unknown_command()
 
 
 def clear_last_lines(n=1000):
@@ -261,71 +322,12 @@ def unknown_command():
 
 
 def main():
-
     global command
     global commandarr
     global commandoriginal
     command = input("".join(f"{neon_colors[i % len(neon_colors)]}{char}" for i, char in enumerate('>>> ')) + "\033[0m").strip()
 
-    commandarr = [n for n in command.lower().split(' ') if n != '']
-    commandoriginal = command.strip()
-    command = command.lower().strip()
-    # If the command is empty, return
-    if command == '':
-        clear_last_lines(1)
-        return
-    # To Do App Commands:
-    if commandarr[0] == 'todo' and len(commandarr) > 1:
-        if commandarr[1] == 'help':
-            todo_help()
-        elif commandarr[1] == 'ls':
-            todo_ls()
-        elif commandarr[1] == 'rm':
-            if len(commandarr) > 2 and commandarr[2] == 'all':
-                todo_list.clear()
-                todo_save_todos()
-                print('All items were deleted.')
-            else:
-                todo_delete_function()
-        elif commandarr[1] == 'add':
-            todo_add()
-        elif commandarr[1] == 'changeorder':
-            todo_changeorder()
-        elif commandarr[1] == 'abcorder':
-            todo_abcorder()
-        elif commandarr[1] == 'cbaorder':
-            todo_cbaorder()
-        elif commandarr[1] == 'do':
-            todo_do_function()
-        else:
-            # Print the closest command if the command is not recognized
-            unknown_command()
-    elif commandarr[0] == 'todo' and len(commandarr) == 1:
-        todo_help()
-    elif commandarr[0] == 'open':
-        open_function()
-    elif commandarr[0] == 'help' and len(commandarr) == 1:
-        print(help_content)
-    elif commandarr[0] == 'chat' and len(commandarr) == 1:
-        chat_function()
-    elif (commandarr[0] == 'exit' or commandarr[0] == 'quit') and len(commandarr) == 1:
-        clear_screen()
-        print("".join(f"{neon_colors[i % len(neon_colors)]}{char}" for i, char in enumerate('\nGoodbye! | Robot Human Assist By: Batu Koray Masak')))
-        sys.exit(0)
-    elif (commandarr[0] == 'clear' or command == 'clr') and len(commandarr) == 1:
-        clear_screen()
-    elif commandarr[0] == 'eval':
-        try:
-            result = eval(command[5:].replace('pi', str(math.pi)).replace('e',str(math.e)))
-            if isinstance(result, (int, float)):
-                print(f"{result:,}")
-            else:
-                print(result)
-        except Exception:
-            unknown_command()
-    else:
-        unknown_command()
-
+    analyze_input(command)
 
 if __name__ == "__main__":
     while True:
